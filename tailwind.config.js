834ea56086abf8326db46ba2949d132a12386048
Velizar Dabov <velizardabov@gmail.com>
2024-11-25 20:55:26 +0200

    hero section started

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,27 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.css",
   ],
   theme: {
-    extend: {},
+    extend: {
+  colors:{
+    blue:{
+      50:'#dfdff0',
+75:'#dfdff2',
+100:'#f0f2fa',
+200:'#010101',
+300:'#4fb7dd'
+    },
+    violet:{
+      300:'#5724ff'
+    },
+    yellow:{
+      100:'#8e983f',
+      300:'#edff66'
+    }
+  }
+    },
   },
   plugins: [],
 }
