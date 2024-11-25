146443ebe8a3a0aa5eee70cf779ef3c299e49037
Velizar Dabov <velizardabov@gmail.com>
2024-11-25 09:56:04 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-5xl text-orange-500 font-bold">Hello</h1>
+    </main>
+  );
+};
+
+export default App;
