afb458f0784a12827ca288144ca0d23c885f7f07
Velizar Dabov <velizardabov@gmail.com>
2024-11-26 19:06:29 +0200

    navbar ready

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 const Button = ({title,id, rightIcon, leftIcon, containerClass }) => {
   return (
     <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full  px-7 py-3 text-black ${containerClass}`}>
-{leftIcon}
+{leftIcon} 
 <span className='relative inline-flex overflow-hidden text-xs uppercase'>
     <div >
         {title}
     </div>
+    
 </span>
-
+{rightIcon}
     </button>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 const handleMiniVdClick = () => {};
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -49,35 +49,39 @@ const Hero = () => {
     },
     { dependencies: [currentIndex], revertOnUpdate: true }
   );
-useGSAP(() => {
-  gsap.set('#video-frame', {
-    clipPath:'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100% )',
-    borderRadius: '0 0 40% 10%'
-  })
-  gsap.from('#video-frame', {clipPath:'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100% )',
-    borderRadius:'0 0 0 0',
-    ease:'power1.inOut',
-    scrollTrigger:{
-      trigger:'#video-frame',
-      start:'center center',
-      end:'bottom center',
-      scrub:true
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0%, 72% 0%, 90% 90%, 0% 100% )",
+      borderRadius: "0 0 40% 10%",
+    });
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100% )",
+      borderRadius: "0 0 0 0",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
+  useEffect(() => {
+    if (loadedVideos === totalVideos - 1) {
+      setLoadedVideos(false);
     }
-  })
-})
-useEffect(()=> {
-if(loadedVideos=== totalVideos-1){
-  setLoadedVideos(false)
-}
-},[loadedVideos])
+  }, [loadedVideos]);
   return (
     <div className="relative h-dvh w-screen overflow-x-hidden">
-      {isLoading && (<div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violer-50">
-        <div className="three-body">
-        <div className="three-body__dot"/>
-        <div className="three-body__dot"/>
-        <div className="three-body__dot"/>
-        </div></div>)}
+      {isLoading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violer-50">
+          <div className="three-body">
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+            <div className="three-body__dot" />
+          </div>
+        </div>
+      )}
       <div
         id="video-frame"
         className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
@@ -112,7 +116,7 @@ if(loadedVideos=== totalVideos-1){
             src={getVideoSrc(
               currentIndex === totalVideos - 1 ? 1 : currentIndex
             )}
-            autoPlay
+            // autoPlay
             loop
             muted
             className="absolute left-0 size-full object-cover object-center"
