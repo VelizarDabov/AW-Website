ced3f0163cb2f34618a4dc08b356b9af2c5604f6
Velizar Dabov <velizardabov@gmail.com>
2024-11-26 21:36:07 +0200

    all done

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Features from "./components/Features";
+import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden bg-zinc-600">
+    <main className="relative min-h-screen w-screen overflow-x-hidden bg-blue-50">
       <Navbar />
       <Hero />
       <About />
       <Features />
+      <Story />
+      <Contact />
+      <Footer/>
     </main>
   );
 };
